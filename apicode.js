a9464bbaedff9376489f3ddb4ccb86986e5275fd
Muhammad Ali <muhammadali.pete@gmail.com>
2021-02-09 17:41:52 -0700

    started api code

---
+++ apicode.js
@@ -0,0 +1,15 @@
+//This is the token for communicating with NOAA API
+const tok = 'zaZbfcdivRFqAGBAcmbrxYyeaDmRwbRy';
+
+
+/*
+const usefulData = getData();
+
+//https://stackoverflow.com/questions/38884522/why-is-my-asynchronous-function-returning-promise-pending-instead-of-a-val
+usefulData.then(function(result){
+    console.log(result.results)
+    result.results.forEach(element => {
+        console.log(element.name)
+    });
+})
+*/
